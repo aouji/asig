a7f2daacfce53d0f4c11419d7d815df778773c65
Arash <arash.ouji@safe.com>
2014-01-29 13:14:37 -0800

    Refactor voting code

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,59 +1,73 @@
 class VotesController < ApplicationController
 before_action :authenticate_user!, only: [:create,:remove]#,:update,:destroy]
+before_action :findproj, only: [:create,:remove]
 
-	def create
-		@proj=Project.find(params[:project_id])
-		if (current_user.voted_projects.include? @proj)
+  def create
+    if (current_user.voted_projects.include? @proj)
+      changevote
+    else
+      addvote 
+    end
+  end
+
+  def remove
+    if (current_user.voted_projects.include? @proj)
+      current_vote=current_user.votes.find_by(project_id: @proj)
+      if (params[:vote]=='upback' && current_vote.vote_kind=='up')
+        @proj.vote_count-=1
+      elsif (params[:vote]=='downback' && current_vote.vote_kind=='down')
+        @proj.vote_count+=1
+      else
+        redirect_to project_path(@proj), alert: "Something's not right."
+      end
+      current_user.voted_projects.delete @proj
+      @proj.save
+      redirect_to project_path(@proj), notice: "Vote removed."
+    else
+      redirect_to project_path(@proj), alert: "You have not voted yet."
+    end
+  end
+
+  private
+
+  def addvote
+    current_user.voted_projects << @proj
+      vote=current_user.votes.find_by(project_id: @proj)
+      if (params[:vote]=='up')
+        @proj.vote_count+=1
+        vote.vote_kind='up'
+      elsif (params[:vote]=='down')
+        @proj.vote_count-=1
+        vote.vote_kind='down'
+      else
+        redirect_to project_path(@proj), alert: "Something's not right."
+      end
+      savevote vote
+  end
+
+  def changevote
       current_vote=current_user.votes.find_by(project_id: @proj)
       if (params[:vote]=='up' && current_vote.vote_kind=='down')
         current_vote.vote_kind='up'
-        current_vote.save
         @proj.vote_count+=2
-        @proj.save
-        redirect_to project_path(@proj)
+        savevote current_vote
       elsif (params[:vote]=='down' && current_vote.vote_kind=='up')
         current_vote.vote_kind='down'
-        current_vote.save
         @proj.vote_count-=2
-        @proj.save
-        redirect_to project_path(@proj)
+        savevote current_vote
       else
 		    redirect_to project_path(@proj), alert: "Something's not right."
       end
-    else
-			current_user.voted_projects << @proj
-			vote=current_user.votes.find_by(project_id: @proj)
-			if (params[:vote]=='up')
-				@proj.vote_count+=1
-				vote.vote_kind='up'
-			elsif (params[:vote]=='down')
-				@proj.vote_count-=1
-				vote.vote_kind='down'
-			else
-				redirect_to project_path(@proj), alert: "Something's not right."
-			end
-			vote.save
-			@proj.save
-			redirect_to project_path(@proj)
-		end
-	end
+  end
 
-	def remove
-		@proj=Project.find(params[:project_id])
-    if (current_user.voted_projects.include? @proj)
-      current_vote=current_user.votes.find_by(project_id: @proj)
-			if (params[:vote]=='upback' && current_vote.vote_kind=='up')
-				@proj.vote_count-=1
-			elsif (params[:vote]=='downback' && current_vote.vote_kind=='down')
-				@proj.vote_count+=1
-			else
-				redirect_to project_path(@proj), alert: "Something's not right."
-			end
-      current_user.voted_projects.delete @proj
-			@proj.save
-			redirect_to project_path(@proj), notice: "Vote removed."
-		else
-			redirect_to project_path(@proj), alert: "You have not voted yet."
-    end
-	end
+  def savevote vote
+    vote.save
+    @proj.save
+    redirect_to project_path(@proj)
+  end
+
+  def findproj
+    @proj=Project.find(params[:project_id])
+  end
+
 end
